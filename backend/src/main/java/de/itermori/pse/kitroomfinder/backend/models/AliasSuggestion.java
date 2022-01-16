bddd83ab9ac9b01b0aafd3df592a43cee4797710
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-16 22:02:16 +0100

    Add column 'voters' to model 'AliasSuggestion'
    Add column 'voters' to model 'AliasSuggestion'.
    Add required getter and setter methods.
    Add required imports
    'javax.persistence.ElementCollection',
    'java.util.List',
    'java.util.ArrayList'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/AliasSuggestion.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/AliasSuggestion.java
@@ -1,6 +1,9 @@
 package de.itermori.pse.kitroomfinder.backend.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,6 +23,9 @@ public class AliasSuggestion {
 
     @Column(name = "appMapID", nullable = false)
     private Integer appMapID;
+
+    @ElementCollection
+    private List<String> voters = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -51,4 +57,14 @@ public class AliasSuggestion {
     private void setAppMapID(Integer appMapID) {
         this.appMapID = appMapID;
     }
+
+    public List<String> getVoters() {
+        return voters;
+    }
+
+    // Hibernate requires the setter for reading values from the database.
+    private void setVoters(List<String> voters) {
+        this.voters = voters;
+    }
+    
 }
