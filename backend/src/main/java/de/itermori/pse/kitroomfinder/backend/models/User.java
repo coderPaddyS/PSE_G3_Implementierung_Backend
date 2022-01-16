4627cfc92a33be3cb01f4e5c0f38cb00c69dfe74
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-16 22:49:06 +0100

    Add column 'name' to model 'User'
    Add column 'name' to model 'User'.
    Add required getter and setter methods.
    Add required import
    'javax.persistence.Column'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
@@ -1,5 +1,6 @@
 package de.itermori.pse.kitroomfinder.backend.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,8 +12,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = 'name', nullable = false)
+    private String name;
+
     public Long getId() {
         return id;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    // Hibernate requires the setter for reading values from the database.
+    private void setName(String name) {
+        this.name = name;
+    }
+
 }
