f18adcc5c76e18dea86b664ea6766e88e80173ec
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-16 22:46:41 +0100

    Add primary key to model 'User'
    Add primary key to model 'User'.
    Add required getter method.
    Add required imports
    'javax.persistence.Id',
    'javax.persistence.GeneratedValue',

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
@@ -1,7 +1,18 @@
 package de.itermori.pse.kitroomfinder.backend.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+    
 }
