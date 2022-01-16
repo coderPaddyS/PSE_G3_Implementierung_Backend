2775296e11e39736d945f4911df7df3945cd02f2
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-16 22:50:13 +0100

    Fix quotations in column 'name' in model 'User'
    Fix quotations (from '' to "")
    in column 'name' in model 'User'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
@@ -12,7 +12,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = 'name', nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
     public Long getId() {
