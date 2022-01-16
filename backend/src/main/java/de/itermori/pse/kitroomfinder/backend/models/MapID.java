cec5745325109dfafd452bb6a7b4157a04e15495
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-16 21:16:44 +0100

    Add standard constructor to 'MapID'
    Add standard constructor explicitly
    to 'MapID' as Hibernate needs it.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/MapID.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/MapID.java
@@ -19,6 +19,10 @@ public class MapID {
     @Column(name = "app_id", nullable = false)
     private Integer appID;
 
+    // Hibernate requires an explicit written standard constructor.
+    public MapID() {
+    }
+
     public Long getId() {
         return id;
     }
