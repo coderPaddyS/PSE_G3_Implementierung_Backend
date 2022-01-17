abb31a8a1d1bcf4a471431cbde21325ff40d6c30
lukas333333 <84440453+lukas333333@users.noreply.github.com>
2022-01-17 12:53:23 +0100

    Update AliasService.java

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasService.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasService.java
@@ -2,9 +2,9 @@ package de.itermori.pse.kitroomfinder.backend.services;
 
 public interface AliasService {
 
-    public Boolean addAlias(String alias, int mapID);
+    public boolean addAlias(String alias, int mapID);
     public Iterable<String> getAlias(int mapID);
     public Iterable<String> getAlias(int mapID, String user);
     public AliasUpdate getAliasUpdates(int version);
-    public Boolean removeAlias(String name);
+    public boolean removeAlias(String name);
 }
