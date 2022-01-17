d5bfe3217eac95c1a8665689ab22746949ab87bd
SCastroAdriano <78550186+SCastroAdriano@users.noreply.github.com>
2022-01-17 12:59:32 +0100

    Fix typo in 'UserService'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/UserService.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/UserService.java
@@ -3,6 +3,6 @@ package de.itermori.pse.kitroomfinder.backend.services;
 public interface UserService {
 
     public boolean addUser(String accessToken);
-    public User loadUserVyToken(String accessToken);
+    public User loadUserByToken(String accessToken);
 
 }
