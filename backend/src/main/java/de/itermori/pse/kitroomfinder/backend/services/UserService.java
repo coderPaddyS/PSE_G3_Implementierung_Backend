c2c9c777c04df441f6f4783e4697329ebdfa7073
lukas333333 <84440453+lukas333333@users.noreply.github.com>
2022-01-17 12:54:29 +0100

    Update UserService.java

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/UserService.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/UserService.java
@@ -2,7 +2,7 @@ package de.itermori.pse.kitroomfinder.backend.services;
 
 public interface UserService {
 
-    public Boolean addUser(String accessToken);
+    public boolean addUser(String accessToken);
     public User loadUserVyToken(String accessToken);
 
 }
