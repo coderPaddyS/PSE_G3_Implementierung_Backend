4a50f7c06c3449e6537ab0982602d1b0b70466c3
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 21:14:49 +0100

    Add constructor to 'AliasServiceImp'
    Add constructor to 'AliasServiceImp'.
    Add annotation 'Autowired' to constructor.
    Add required import 'org.springframework.
    beans.factory.annotation.Autowired'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasServiceImp.java
@@ -1,10 +1,16 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
 import de.itermori.pse.kitroomfinder.backend.repositories.AliasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AliasServiceImp implements AliasService {
 
     private AliasRepository aliasRepository;
+
+    @Autowired
+    public AliasServiceImp(AliasRepository aliasRepository) {
+        this.aliasRepository = aliasRepository;
+    }
 }
