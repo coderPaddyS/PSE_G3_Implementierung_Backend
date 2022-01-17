4f1b2d8edd64669cdca9de8f7185109bb7c0ae5d
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 21:11:41 +0100

    Add attributes to 'AliasServiceImp'
    Add attribute 'aliasRepository' of
    type 'AliasRepository'.
    Add required import
    'de.itermori.pse.kitroomfinder.backend.
    repositories.AliasRepository'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasServiceImp.java
@@ -1,7 +1,10 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
+import de.itermori.pse.kitroomfinder.backend.repositories.AliasRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AliasServiceImp implements AliasService {
+
+    private AliasRepository aliasRepository;
 }
