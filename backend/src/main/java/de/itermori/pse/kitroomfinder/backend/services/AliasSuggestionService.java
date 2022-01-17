0b7154cce76458e422d5700bc7ca10a6da640db0
lukas333333 <84440453+lukas333333@users.noreply.github.com>
2022-01-17 12:53:39 +0100

    Update AliasSuggestionService.java

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionService.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionService.java
@@ -2,9 +2,9 @@ package de.itermori.pse.kitroomfinder.backend.services;
 
 public interface AliasSuggestionService {
 
-    public Boolean addAliasSuggestoin(String aliasSuggestion, int mapID, String user);
-    public Boolean removeAliasSuggestion(String aliasSuggestion, int mapID);
-    public Boolean voteForAlias(String aliasSuggestion, int mapID, String user, boolean vote);
+    public boolean addAliasSuggestoin(String aliasSuggestion, int mapID, String user);
+    public boolean removeAliasSuggestion(String aliasSuggestion, int mapID);
+    public boolean voteForAlias(String aliasSuggestion, int mapID, String user, boolean vote);
     public Iterable<AliasSuggestion> getAliasSuggestion(int minValToShowPos, int minValToShowNeg);
 
 }
