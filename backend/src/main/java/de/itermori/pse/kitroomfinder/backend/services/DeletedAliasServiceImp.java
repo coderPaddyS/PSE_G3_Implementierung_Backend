f2b42e6d8be158fca2d254da2d47dd1cab230b6f
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 22:59:55 +0100

    Add constructor to 'DeletedAliasServiceImp'
    Add constructor to 'DeletedAliasServiceImp'.
    Add annotation 'Autowired' to constructor.
    Add required import 'org.springframework.
    beans.factory.annotation.Autowired'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/DeletedAliasServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/DeletedAliasServiceImp.java
@@ -1,10 +1,16 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
 import de.itermori.pse.kitroomfinder.backend.repositories.DeletedAliasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeletedAliasServiceImp implements DeletedAliasService {
 
     private DeletedAliasRepository deletedAliasRepository;
+
+    @Autowired
+    public DeletedAliasServiceImp(DeletedAliasRepository deletedAliasRepository) {
+        this.deletedAliasRepository = deletedAliasRepository;
+    }
 }
