e52df1f886068d88edb4a121d8790200f7689934
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 22:57:00 +0100

    Define 'DeletedAliasServiceImp' as imp of 'DeletedAliasService'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/DeletedAliasServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/DeletedAliasServiceImp.java
@@ -3,5 +3,5 @@ package de.itermori.pse.kitroomfinder.backend.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DeletedAliasServiceImp {
+public class DeletedAliasServiceImp implements DeletedAliasService {
 }
