24d932cd747c2efe9ab8327c738fd54a25e5ec83
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 22:47:20 +0100

    Fix method return type in 'AliasService(Imp)'
    Fix return type of method
    'getAliasUpdates()' in 'AliasService(Imp)'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasService.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasService.java
@@ -1,13 +1,12 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
 import de.itermori.pse.kitroomfinder.backend.models.Alias;
-import de.itermori.pse.kitroomfinder.backend.utilwrapper.AliasUpdate;
 
 public interface AliasService {
 
     public boolean addAlias(String alias, int mapID);
     public Iterable<Alias> getAlias(int mapID);
     public Iterable<Alias> getAlias(int mapID, String user);
-    public AliasUpdate getAliasUpdates(int version);
+    public Iterable<Alias> getAliasUpdates(int version);
     public boolean removeAlias(String name);
 }
