616489e7e55a0344dce9a9f6eef263b43d13aeff
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 23:12:53 +0100

    Define 'AliasSuggestionServiceImp' as imp of 'AliasSuggestionService'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceImp.java
@@ -3,5 +3,5 @@ package de.itermori.pse.kitroomfinder.backend.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AliasSuggestionServiceImp {
+public class AliasSuggestionServiceImp implements AliasSuggestionService {
 }
