26602d93d1bc37280695bbbbfb3ad7ca6b461613
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 22:57:50 +0100

    Add attributes to 'DeletedAliasServiceImp'
    Add attribute 'deletedAliasRepository'
    of type 'DeletedAliasRepository'.
    Add required import
    'de.itermori.pse.kitroomfinder.backend.
    repositories.DeletedAliasRepository'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/DeletedAliasServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/DeletedAliasServiceImp.java
@@ -1,7 +1,10 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
+import de.itermori.pse.kitroomfinder.backend.repositories.DeletedAliasRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeletedAliasServiceImp implements DeletedAliasService {
+
+    private DeletedAliasRepository deletedAliasRepository;
 }
