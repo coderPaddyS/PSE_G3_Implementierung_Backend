b4021844699bd5ebe848ac1c7a049267b44d0300
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-17 23:13:30 +0100

    Add attributes to 'AliasSuggestionServiceImp'
    Add attribute 'aliasSuggestionRepository'
    of type 'AliasSuggestionRepository'.
    Add required import
    'de.itermori.pse.kitroomfinder.backend.
    repositories.AliasSuggestionRepository'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceImp.java
@@ -1,7 +1,10 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
+import de.itermori.pse.kitroomfinder.backend.repositories.AliasSuggestionRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AliasSuggestionServiceImp implements AliasSuggestionService {
+
+    private AliasSuggestionRepository aliasSuggestionRepository;
 }
