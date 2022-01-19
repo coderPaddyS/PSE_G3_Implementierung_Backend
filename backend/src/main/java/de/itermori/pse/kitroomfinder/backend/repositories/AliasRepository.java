de624219bf384c9f3a9c0e2418a9c3bf8cf73154
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-19 15:35:11 +0100

    Add method 'findUpdatesByVersion()' to 'AliasRepository'
    Add method 'Iterable<Alias> findUpdatesByVersion
    (@Param("version")int)' to 'AliasRepository'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepository.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepository.java
@@ -17,6 +17,9 @@ public interface AliasRepository extends JpaRepository<Alias, Long> {
     public Iterable<Alias> findByName(@Param("alias") String alias);
 
     @Modifying
-    @Query ("DELETE FROM Alias a WHERE a.name=:name")
+    @Query("DELETE FROM Alias a WHERE a.name=:name")
     public boolean deleteByName(@Param("name")String name);
+
+    @Query("SELECT a FROM Alias AS a WHERE a.version>:version")
+    public Iterable<Alias> findUpdatesByVersion(@Param("version")int version);
 }
