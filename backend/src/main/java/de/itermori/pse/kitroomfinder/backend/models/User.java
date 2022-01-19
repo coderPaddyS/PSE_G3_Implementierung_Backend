c78bd8312791d3d861cfbc751bd75650df434e96
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-19 18:34:04 +0100

    Add column 'aliasSuggestion' in 'User.java'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
@@ -4,9 +4,13 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
@@ -20,6 +24,10 @@ public class User {
 
     @ElementCollection
     private Set<? extends GrantedAuthority> authorities;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "aliasSuggestion_id")
+    private AliasSuggestion aliasSuggestion;
 
     // Hibernate requires an explicitly written standard constructor.
     public User() {
