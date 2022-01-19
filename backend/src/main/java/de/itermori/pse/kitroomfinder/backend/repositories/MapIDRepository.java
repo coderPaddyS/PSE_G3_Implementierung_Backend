f5cc9bed77d2aa77d4d44253cfe9e837e5904b3c
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-19 19:02:39 +0100

    Add method 'findByID()' to 'MapIDRepository'
    Add method 'String findByID(@Param("mapID") int)'
    to 'MapIDRepository'.

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepository.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepository.java
@@ -2,6 +2,12 @@ package de.itermori.pse.kitroomfinder.backend.repositories;
 
 import de.itermori.pse.kitroomfinder.backend.models.MapID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MapIDRepository extends JpaRepository<MapID, Long> {
+
+    @Query("SELECT m FROM MapID AS m WHERE m.mapID=:alias")
+    public String findByID(@Param("mapID") int mapID);
+
 }
