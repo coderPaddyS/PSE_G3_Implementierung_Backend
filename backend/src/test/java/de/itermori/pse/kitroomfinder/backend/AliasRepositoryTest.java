ff9e565ee31702971d39a01612d76cc9e6835f5c
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-20 23:12:28 +0100

    Add required attribute 'aliasRepository' to 'AliasRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/AliasRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/AliasRepositoryTest.java
@@ -1,7 +1,10 @@
 package de.itermori.pse.kitroomfinder.backend;
 
+import de.itermori.pse.kitroomfinder.backend.repositories.AliasRepository;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AliasRepositoryTest {
+
+    private AliasRepository aliasRepository;
 }
