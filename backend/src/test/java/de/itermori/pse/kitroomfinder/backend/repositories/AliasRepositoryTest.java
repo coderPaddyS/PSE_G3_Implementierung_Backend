539bc79e0498ca1f1883378479cc021236ef9026
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-21 00:47:26 +0100

    Add 'TestPropertySource' to 'AliasRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
@@ -2,8 +2,12 @@ package de.itermori.pse.kitroomfinder.backend.repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 public class AliasRepositoryTest {
 
     @Autowired
