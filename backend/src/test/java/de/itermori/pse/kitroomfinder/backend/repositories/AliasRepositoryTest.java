8bcca7cce3abbc88b475b43a97c90de1b4b822bc
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-21 00:46:48 +0100

    Add annotation '@Autowired' to attribute in 'AliasRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
@@ -1,9 +1,11 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AliasRepositoryTest {
 
+    @Autowired
     private AliasRepository aliasRepository;
 }
