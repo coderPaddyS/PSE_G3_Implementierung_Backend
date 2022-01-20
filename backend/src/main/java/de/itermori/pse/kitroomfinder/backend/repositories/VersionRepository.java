4cd1794fd23a12e5f7755bf4103bb68de09732ed
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-20 15:05:33 +0100

    Add repository 'VersionRepository'

---
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/VersionRepository.java
@@ -0,0 +1,14 @@
+package de.itermori.pse.kitroomfinder.backend.repositories;
+
+import de.itermori.pse.kitroomfinder.backend.models.Version;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface VersionRepository extends JpaRepository<Version, Long> {
+
+    @Modifying
+    @Query("UPDATE Version a SET a.currentVersion = a.currentVersion + 1")
+    public void incrementVersion();
+
+}
