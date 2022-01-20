ed9b8dddabf8bddb8ea7521644432e2e0a3d5f0f
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-20 23:12:01 +0100

    Define 'AliasRepositoryTest' as 'SpringBootTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/AliasRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/AliasRepositoryTest.java
@@ -1,4 +1,7 @@
 package de.itermori.pse.kitroomfinder.backend;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AliasRepositoryTest {
 }
