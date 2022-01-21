b230dce2dce6053b72e3ff639ebc001baa40d2ed
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-21 01:04:14 +0100

    Add 'whenAliasSaved_thenFindsByMapID()' to 'AliasRepositoryTest'
    Add test method 'void whenAliasSaved_thenFindsByMapID()'
    to 'AliasRepositoryTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
@@ -1,15 +1,37 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import de.itermori.pse.kitroomfinder.backend.models.Alias;
+import java.util.Iterator;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
         "spring.jpa.hibernate.ddl-auto=create-drop"
 })
-public class AliasRepositoryTest {
+class AliasRepositoryTest {
 
     @Autowired
     private AliasRepository aliasRepository;
+
+    @Test
+    void whenAliasSaved_thenFindsByMapID() {
+        Alias expectedAlias = new Alias("Infobau", 1, 1);
+        aliasRepository.save(expectedAlias);
+        Iterable<Alias> actualAliases = aliasRepository.findByMapID(1);
+        Iterator<Alias> aliasIterator = actualAliases.iterator();
+        int actualAmountAliases = 0;
+        Alias actualAlias = new Alias("AnotherAlias", 2, 2);
+        while (aliasIterator.hasNext()) {
+            ++actualAmountAliases;
+            actualAlias = aliasIterator.next(); // now correct value should be saved in actualAlias
+        }
+        assertEquals(1, actualAmountAliases);
+        assertEquals(expectedAlias, actualAlias);
+    }
+
 }
