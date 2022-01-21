10506aba57bdb0af8ce916dcd714e5307cae929a
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-21 22:30:00 +0100

    Fix modifying query methods in 'AliasSuggestionRepository'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasSuggestionRepository.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasSuggestionRepository.java
@@ -5,14 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AliasSuggestionRepository extends JpaRepository<AliasSuggestion, Long> {
 
     @Modifying
+    @Transactional
     @Query("DELETE FROM AliasSuggestion a WHERE a.name=:alias AND a.mapID=:mapID")
     public void deleteByNameAndID(@Param("mapID")int mapID, @Param("alias")String alias);
 
     @Modifying
+    @Transactional
     @Query("DELETE FROM AliasSuggestion a WHERE a.name=:alias")
     public void deleteByName(@Param("alias")String alias);
 
@@ -20,11 +23,13 @@ public interface AliasSuggestionRepository extends JpaRepository<AliasSuggestion
     public Iterable<AliasSuggestion> findByVotes(@Param("minVotesNeg") int minVotesNeg,
                                                  @Param("minVotesPos") int minVotesPos);
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
+    @Transactional
     @Query("UPDATE AliasSuggestion a SET a.posVotes = a.posVotes + 1 WHERE a.mapID=:mapID AND a.name=:alias")
     public void votePos(@Param("mapID") int mapID, @Param("alias")String alias);
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
+    @Transactional
     @Query("UPDATE AliasSuggestion a SET a.negVotes = a.negVotes + 1 WHERE a.mapID=:mapID AND a.name=:alias")
     public void voteNeg(@Param("mapID") int mapID, @Param("alias")String alias);
 
