964605bf17246ca8fec84ffa0141a39997debdb8
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-21 08:12:30 +0100

    Add method 'whenAliasSaved_thenDeleteByName()' to 'AliasRepositoryTest'
    Add method 'void whenAliasSaved_thenDeleteByName()'
    to 'AliasRepositoryTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
@@ -48,6 +49,37 @@ class AliasRepositoryTest {
         }
         assertEquals(1, actualAmountAliases);
         assertEquals(expectedAlias, actualAlias);
+    }
+
+    @Test
+    void whenAliasSaved_thenDeleteByName() {
+        // first save alias in database
+        Alias toDelete = new Alias("Infobau", 1, 1);
+        aliasRepository.save(toDelete);
+
+        // now check if it is saved in database
+        Iterable<Alias> actualAliases = aliasRepository.findByName("Infobau");
+        Iterator<Alias> aliasIterator = actualAliases.iterator();
+        int actualAmountAliases = 0;
+        Alias actualAlias = new Alias("AnotherAlias", 2, 2);
+        while (aliasIterator.hasNext()) {
+            ++actualAmountAliases;
+            actualAlias = aliasIterator.next(); // now correct value should be saved in actualAlias
+        }
+        assertEquals(1, actualAmountAliases);
+        assertEquals(toDelete, actualAlias);
+
+        // now delete it
+        aliasRepository.deleteByName(toDelete.getName());
+
+        Alias actualAliasesSaved = null;
+        actualAmountAliases = 0;
+        while (aliasIterator.hasNext()) {
+            ++actualAmountAliases;
+            actualAlias = aliasIterator.next(); // now correct value should be saved in actualAlias
+        }
+        assertEquals(0, actualAmountAliases);
+        assertNull(actualAliasesSaved);
     }
 
 }
