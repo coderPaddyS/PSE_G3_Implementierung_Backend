d163d093f8cffc521df3d18b917865ca60464bdf
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-21 08:31:39 +0100

    Add test class 'AliasSuggestionRepositoryTest'

---
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasSuggestionRepositoryTest.java
@@ -0,0 +1,11 @@
+package de.itermori.pse.kitroomfinder.backend.repositories;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+class AliasSuggestionRepositoryTest {
+}
