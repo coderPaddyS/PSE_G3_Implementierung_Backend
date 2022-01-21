e123b2b901ec84f3f29da28ec74e50c91f91fe8d
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-21 18:02:47 +0100

    Add 'whenAliasSuggestionSaved_findByNameAndMapID()' to 'AliasSuggestionRepositoryTest'
    Add method 'void whenAliasSuggestionSaved_findByNameAndMapID()'
    to 'AliasSuggestionRepositoryTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasSuggestionRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/AliasSuggestionRepositoryTest.java
@@ -1,5 +1,9 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import de.itermori.pse.kitroomfinder.backend.models.AliasSuggestion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -8,4 +12,15 @@ import org.springframework.test.context.TestPropertySource;
         "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class AliasSuggestionRepositoryTest {
+
+    @Autowired
+    private AliasSuggestionRepository aliasSuggestionRepository;
+
+    @Test
+    void whenAliasSuggestionSaved_findByNameAndMapID() {
+        AliasSuggestion expectedAliasSuggestion = new AliasSuggestion("HSaF", 1, "Suggester");
+        aliasSuggestionRepository.save(expectedAliasSuggestion);
+        AliasSuggestion actualAliasSuggestion = aliasSuggestionRepository.findByNameAndMapID("HSaF", 1);
+        Assertions.assertEquals(expectedAliasSuggestion, actualAliasSuggestion);
+    }
 }
