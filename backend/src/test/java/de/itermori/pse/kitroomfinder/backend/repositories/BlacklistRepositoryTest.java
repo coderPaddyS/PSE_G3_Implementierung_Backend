62760e6795d7a14212a27d4282da808fc1af99ce
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 15:26:41 +0100

    Add 'void setUp()' to 'BlacklistRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
@@ -1,5 +1,6 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,4 +9,9 @@ public class BlacklistRepositoryTest {
 
     @Autowired
     private BlacklistRepository blacklistRepository;
+
+    @BeforeEach
+    void setUp() {
+        blacklistRepository.deleteAll();
+    }
 }
