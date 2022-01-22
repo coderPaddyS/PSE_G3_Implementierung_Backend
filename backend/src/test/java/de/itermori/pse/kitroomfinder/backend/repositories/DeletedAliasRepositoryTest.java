ee6790a315abfffae5f973bbc347541da04f9f11
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 15:54:13 +0100

    Add 'void setUp()' to 'DeletedAliasRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/DeletedAliasRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/DeletedAliasRepositoryTest.java
@@ -1,11 +1,18 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class DeletedAliasRepositoryTest {
+class DeletedAliasRepositoryTest {
 
     @Autowired
     private DeletedAliasRepository deletedAliasRepository;
+
+    @BeforeEach
+    void setUp() {
+        deletedAliasRepository.deleteAll();
+    }
+    
 }
