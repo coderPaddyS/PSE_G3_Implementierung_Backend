dde780a65911c1298f51a6c66071331a8de5d5ba
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 17:04:44 +0100

    Add missing annotation 'Transactional' to 'VersionRepository'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/VersionRepository.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/VersionRepository.java
@@ -4,10 +4,12 @@ import de.itermori.pse.kitroomfinder.backend.models.Version;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface VersionRepository extends JpaRepository<Version, Long> {
 
     @Modifying
+    @Transactional
     @Query("UPDATE Version a SET a.currentVersion = a.currentVersion + 1")
     public void incrementVersion();
 
