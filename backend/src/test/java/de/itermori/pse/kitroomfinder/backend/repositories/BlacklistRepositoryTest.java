7882b0f3f2e988170838be651992e23277a4da5b
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 15:33:55 +0100

    Add 'whenBlacklisted_deleteByName()' to 'BlacklistRepositoryTest'
    Add 'void whenBlacklisted_deleteByName()'
    to 'BlacklistRepositoryTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
@@ -1,11 +1,17 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import de.itermori.pse.kitroomfinder.backend.models.BlacklistEntry;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class BlacklistRepositoryTest {
+class BlacklistRepositoryTest {
 
     @Autowired
     private BlacklistRepository blacklistRepository;
@@ -14,4 +20,24 @@ public class BlacklistRepositoryTest {
     void setUp() {
         blacklistRepository.deleteAll();
     }
+
+    @Test
+    void whenBlacklisted_deleteByName() {
+        // save a BlacklistEntry to the database
+        BlacklistEntry blacklistEntry = new BlacklistEntry("forbidden");
+        blacklistRepository.save(blacklistEntry);
+
+        // check if blacklistEntry is saved in the database
+        List<BlacklistEntry> blacklistEntries = blacklistRepository.findAll();
+        assertEquals(1, blacklistEntries.size());
+        assertEquals(blacklistEntry, blacklistEntries.get(0));
+
+        // delete blacklistEntry
+        blacklistRepository.deleteByName(blacklistEntry.getName());
+
+        // check if blacklistEntry is no longer saved in the database
+        blacklistEntries = blacklistRepository.findAll();
+        assertTrue(blacklistEntries.isEmpty());
+    }
+    
 }
