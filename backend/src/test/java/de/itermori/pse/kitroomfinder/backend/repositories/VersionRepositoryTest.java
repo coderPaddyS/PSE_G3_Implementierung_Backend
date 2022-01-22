a516c9f490de79a3c8af3673d1fdc3764b8f959b
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 16:49:54 +0100

    Add 'void setUp()' to 'VersionRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/VersionRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/VersionRepositoryTest.java
@@ -1,5 +1,6 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,4 +9,10 @@ class VersionRepositoryTest {
 
     @Autowired
     private VersionRepository versionRepository;
+
+    @BeforeEach
+    void setUp() {
+        versionRepository.deleteAll();
+    }
+    
 }
