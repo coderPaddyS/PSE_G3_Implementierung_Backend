687dd2225e9ce216fda55120986fe3b37d45a445
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 15:24:47 +0100

    Add attribute 'blacklistRepository' to 'BlacklistRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
@@ -1,7 +1,11 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BlacklistRepositoryTest {
+
+    @Autowired
+    private BlacklistRepository blacklistRepository;
 }
