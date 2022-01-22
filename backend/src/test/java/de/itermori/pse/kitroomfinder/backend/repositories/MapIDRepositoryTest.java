c87d1ea4fd3a167863ce74c4402a219f263a9157
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 16:24:28 +0100

    Add 'whenMapIDSaved_thenFindByID()' to 'MapIDRepositoryTest'
    Add 'void whenMapIDSaved_thenFindByID()'
    to 'MapIDRepositoryTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepositoryTest.java
@@ -1,8 +1,12 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import de.itermori.pse.kitroomfinder.backend.models.MapID;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MapIDRepositoryTest {
@@ -13,6 +17,17 @@ class MapIDRepositoryTest {
     @BeforeEach
     void setUp() {
         mapIDRepository.deleteAll();
+    }
+
+    @Test
+    void whenMapIDSaved_thenFindByID() {
+        // save MapID to the database
+        MapID expectedMapID = new MapID("50.34", 1);
+        mapIDRepository.save(expectedMapID);
+
+        // check if saved MapID is found
+        String actualMapIDName = mapIDRepository.findByID(expectedMapID.getMapID());
+        assertEquals(expectedMapID.getName(), actualMapIDName);
     }
     
 }
