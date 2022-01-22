d08f85fb2fb39eda7a435000f6b8832bf0d54729
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 16:53:54 +0100

    Add 'whenVersionAtCertainValue_thenIncrementVersion()'
    Add 'void whenVersionAtCertainValue_thenIncrementVersion()'
    to 'VersionRepositoryTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/VersionRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/VersionRepositoryTest.java
@@ -1,8 +1,12 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import de.itermori.pse.kitroomfinder.backend.models.Version;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class VersionRepositoryTest {
@@ -14,5 +18,12 @@ class VersionRepositoryTest {
     void setUp() {
         versionRepository.deleteAll();
     }
-    
+
+    @Test
+    void whenVersionAtCertainValue_thenIncrementVersion() {
+        int currentVersion = 1;
+        Version version = new Version(currentVersion);
+        versionRepository.incrementVersion();
+        assertEquals(++currentVersion, versionRepository.retrieveCurrentVersion());
+    }
 }
