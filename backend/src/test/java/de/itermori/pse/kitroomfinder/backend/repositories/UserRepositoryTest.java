89331628abef893a4fee77fc6d7e3d548e60a65b
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 16:34:21 +0100

    Add attribute 'userRepository' to 'UserRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/UserRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/UserRepositoryTest.java
@@ -1,4 +1,11 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
 }
