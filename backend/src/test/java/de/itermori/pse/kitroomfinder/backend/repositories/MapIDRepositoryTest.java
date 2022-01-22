0bd476eba410978e9a2d567ad9ffd0c0c659a481
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 16:26:15 +0100

    Add 'whenMapIDSaved_thenFindByName()' to 'MapIDRepositoryTest'
    Add 'void whenMapIDSaved_thenFindByName()'
    to 'MapIDRepositoryTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepositoryTest.java
@@ -29,5 +29,16 @@ class MapIDRepositoryTest {
         String actualMapIDName = mapIDRepository.findByID(expectedMapID.getMapID());
         assertEquals(expectedMapID.getName(), actualMapIDName);
     }
-    
+
+    @Test
+    void whenMapIDSaved_thenFindByName() {
+        // save MapID to the database
+        MapID expectedMapID = new MapID("50.34", 1);
+        mapIDRepository.save(expectedMapID);
+
+        // check if saved MapID is found
+        int actualMapID = mapIDRepository.findByName(expectedMapID.getName());
+        assertEquals(expectedMapID.getMapID(), actualMapID);
+    }
+
 }
