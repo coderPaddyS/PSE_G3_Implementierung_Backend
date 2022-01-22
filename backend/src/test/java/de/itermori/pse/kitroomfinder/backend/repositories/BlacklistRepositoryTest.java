84c4db8067f451071a2042a9b6aeda7ff8e13910
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 15:24:05 +0100

    Add 'SpringBootTest' annotation to 'BlacklistRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepositoryTest.java
@@ -1,4 +1,7 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BlacklistRepositoryTest {
 }
