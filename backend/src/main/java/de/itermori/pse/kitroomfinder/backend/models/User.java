56581d8e7246309b7857998abd288733fbf66ef5
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 16:44:07 +0100

    Add 'equals()', 'hashCode()' to 'User'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/models/User.java
@@ -1,6 +1,7 @@
 package de.itermori.pse.kitroomfinder.backend.models;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -62,6 +63,23 @@ public class User {
     @Override
     public String toString() {
         return this.getClass().getSimpleName() + "-" + this.getId();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return getId().equals(user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
     
 }
