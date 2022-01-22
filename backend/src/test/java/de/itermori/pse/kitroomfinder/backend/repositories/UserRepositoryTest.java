48775cd0aaf70608863c14c37d86c95506c48ed9
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-01-22 16:34:56 +0100

    Add 'void setUp()' to 'UserRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/UserRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,4 +9,10 @@ public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+    
 }
