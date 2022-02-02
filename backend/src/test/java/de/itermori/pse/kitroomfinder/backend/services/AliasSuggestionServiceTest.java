08471921cca0b010fcf789bae3173057bac67c0b
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-02 09:14:14 +0100

    Add 'whenAliasSuggestionNotYetAdded_thenAddAliasSuggestion()'
    Add 'void whenAliasSuggestionNotYetAdded_thenAddAliasSuggestion()'
    to 'AliasSuggestionServiceTest'.

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceTest.java
@@ -1,15 +1,35 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
+import de.itermori.pse.kitroomfinder.backend.models.AliasSuggestion;
 import de.itermori.pse.kitroomfinder.backend.repositories.AliasSuggestionRepository;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AliasSuggestionServiceTest {
+class AliasSuggestionServiceTest {
 
     @Autowired
     private AliasSuggestionService aliasSuggestionService;
 
     @Autowired
     private AliasSuggestionRepository aliasSuggestionRepository;
+
+    @Test
+    void whenAliasSuggestionNotYetAdded_thenAddAliasSuggestion() {
+        // add alias suggestion
+        assertTrue(aliasSuggestionService.addAliasSuggestion("HSaF", 1, "user"));
+
+        // check if alias suggestion was added to database
+        List<AliasSuggestion> savedAliasSuggestions = aliasSuggestionRepository.findAll();
+        assertEquals(1, savedAliasSuggestions.size());
+        assertEquals("HSaF", savedAliasSuggestions.get(0).getName());
+        assertEquals(1, savedAliasSuggestions.get(0).getMapID());
+        assertEquals("user", savedAliasSuggestions.get(0).getSuggester());
+    }
 }
