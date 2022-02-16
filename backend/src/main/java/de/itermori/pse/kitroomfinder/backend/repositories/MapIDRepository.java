a35944e0d8e8e41f39a3fc11ac869ef434bcb054
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-16 22:15:23 +0100

    Add javadoc to 'MapIDRepository'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepository.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/MapIDRepository.java
@@ -5,12 +5,37 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+/**
+ * Provides a {@link JpaRepository} interface for the model {@link MapID}.
+ * Communicates with the database, more precisely with the table regarding the MapIDs.
+ *
+ * @author Lukas Zetto
+ * @author Adriano Castro
+ * @version 1.0
+ */
 public interface MapIDRepository extends JpaRepository<MapID, Long> {
 
+    /**
+     * Finds the {@link MapID} stored in the database
+     * which has as attribute the mapID provided.
+     *
+     * @param mapID The mapID which the {@link MapID} should have as attribute.
+     * @return      The name of the found {@link MapID} which has as attribute
+     *              the mapID provided.
+     *              If no {@link MapID} is found, null is returned.
+     */
     @Query("SELECT m.name FROM MapID AS m WHERE m.mapID=:mapID")
-    public String findByID(@Param("mapID") int mapID);
+    String findByID(@Param("mapID") int mapID);
 
+    /**
+     * Finds the {@link MapID} stored in the database
+     * which has as attribute the name (mapObject) provided.
+     *
+     * @param mapObject The name (mapObject) which the {@link MapID} should have as attribute.
+     * @return          The mapID of the found {@link MapID} which has as attribute
+     *                  the name (mapObject) provided.
+     */
     @Query("SELECT m.mapID FROM MapID AS m WHERE m.name=:mapObject")
-    public int findByName(@Param("mapObject") String mapObject);
+    int findByName(@Param("mapObject") String mapObject);
 
 }
