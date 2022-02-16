a7c41a9d5edfab671a8a69e5b9f897f3c0638b95
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-16 21:36:39 +0100

    Refactor 'BlacklistRepository'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepository.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/repositories/BlacklistRepository.java
@@ -1,6 +1,5 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
-import de.itermori.pse.kitroomfinder.backend.models.AliasSuggestion;
 import de.itermori.pse.kitroomfinder.backend.models.BlacklistEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
