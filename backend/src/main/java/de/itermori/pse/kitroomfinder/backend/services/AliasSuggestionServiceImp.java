a12c9ea96c221348651c66a11ab3d3d6b27356a9
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-17 10:26:57 +0100

    Add javadoc to 'AliasSuggestionServiceImp'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasSuggestionServiceImp.java
@@ -1,17 +1,36 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
+import de.itermori.pse.kitroomfinder.backend.exceptions.NoSuchAliasSuggestionException;
 import de.itermori.pse.kitroomfinder.backend.models.AliasSuggestion;
+import de.itermori.pse.kitroomfinder.backend.repositories.AliasRepository;
 import de.itermori.pse.kitroomfinder.backend.repositories.AliasSuggestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Provides a service for the model {@link AliasSuggestion}.
+ * Implements the service interface defines the corresponding GraphQL schema methods
+ * related to the model {@link AliasSuggestion}.
+ * Uses the classes {@link AliasSuggestionRepository}, {@link BlacklistService}.
+ *
+ * @author Lukas Zetto
+ * @author Adriano Castro
+ * @version 1.0
+ */
 @Service
 public class AliasSuggestionServiceImp implements AliasSuggestionService {
 
     private final AliasSuggestionRepository aliasSuggestionRepository;
     private final BlacklistService blacklistService;
 
+    /**
+     * The constructor which initializes the alias suggestion service implementation
+     * with the required classes.
+     *
+     * @param aliasSuggestionRepository   The required {@link AliasRepository}.
+     * @param blacklistService            The required {@link BlacklistService}.
+     */
     @Autowired
     public AliasSuggestionServiceImp(AliasSuggestionRepository aliasSuggestionRepository,
                                      BlacklistService blacklistService) {
@@ -19,6 +38,9 @@ public class AliasSuggestionServiceImp implements AliasSuggestionService {
         this.blacklistService = blacklistService;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional
     @Override
     public boolean addAliasSuggestion(String aliasSuggestion, int mapID, String mapObject, String user) {
@@ -32,16 +54,19 @@ public class AliasSuggestionServiceImp implements AliasSuggestionService {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional
     @Override
     public boolean removeAliasSuggestion(String aliasSuggestion, int mapID) {
-        if (aliasSuggestionRepository.findByNameAndMapID(aliasSuggestion, mapID) == null){
-            return false;
-        }
         aliasSuggestionRepository.deleteByNameAndID(aliasSuggestion, mapID);
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional
     @Override
     public boolean removeAliasSuggestion(String aliasSuggestion) {
@@ -49,6 +74,9 @@ public class AliasSuggestionServiceImp implements AliasSuggestionService {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional
     @Override
     public boolean voteForAlias(String aliasSuggestion, int mapID, String user, boolean vote) {
@@ -69,28 +97,49 @@ public class AliasSuggestionServiceImp implements AliasSuggestionService {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterable<AliasSuggestion> getAliasSuggestions(int minValToShowPos, int minValToShowNeg) {
         return aliasSuggestionRepository.findByVotes(minValToShowPos, minValToShowNeg);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterable<AliasSuggestion> getAliasSuggestionsAmount(int mapID, int amount, String user) {
         return aliasSuggestionRepository.findAmount( mapID, amount, "%" + user + "%");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getAmountEntriesAliasSuggestion() {
         return String.valueOf(aliasSuggestionRepository.count());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getPosVotes(String aliasSuggestion, int mapID) {
+        if (aliasSuggestionRepository.findByNameAndMapID(aliasSuggestion, mapID) == null) {
+            throw new NoSuchAliasSuggestionException();
+        }
         return aliasSuggestionRepository.getPosVotes(aliasSuggestion, mapID);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getNegVotes(String aliasSuggestion, int mapID) {
+        if (aliasSuggestionRepository.findByNameAndMapID(aliasSuggestion, mapID) == null) {
+            throw new NoSuchAliasSuggestionException();
+        }
         return aliasSuggestionRepository.getNegVotes(aliasSuggestion, mapID);
     }
 
