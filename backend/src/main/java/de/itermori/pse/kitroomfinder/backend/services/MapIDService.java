68e8347c2d6986d77d029e47a0a601a944aeb30f
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-17 11:25:32 +0100

    Add javadoc to 'MapIDService'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/MapIDService.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/MapIDService.java
@@ -1,10 +1,49 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
+import de.itermori.pse.kitroomfinder.backend.models.MapID;
+
+/**
+ * Provides a service for the model {@link MapIDService}.
+ * This service interface defines the corresponding GraphQL schema methods
+ * related to the model {@link MapIDService}.
+ *
+ * @author Lukas Zetto
+ * @author Adriano Castro
+ * @version 1.0
+ */
 public interface MapIDService {
 
-    public String getMapObject(int mapID);
-    public int getMapIDByName(String mapObject);
-    public Iterable<Integer> getAllMapIDs();
-    public Iterable<String> getAllMapIDsName();
+    /**
+     * Returns the name (mapObject) of
+     * the desired {@link MapID} stored in the database.
+     *
+     * @param mapID The mapID of the desired {@link MapID} stored in the database.
+     * @return      The name (mapObject) of the desired {@link MapID} stored in the database.
+     */
+    String getMapObject(int mapID);
+
+    /**
+     * Returns the mapID of
+     * the desired {@link MapID} stored in the database.
+     *
+     * @param mapObject The name (mapObject) of the desired {@link MapID} stored in the database.
+     * @return          The mapID of the desired {@link MapID} stored in the database.
+     */
+    int getMapIDByName(String mapObject);
+
+    /**
+     * Returns the mapIDs of all MapIDs (type: {@link MapID}) stored in the database.
+     *
+     * @return  An {@link Iterable} of all the mapIDs of all MapIDs (type: {@link MapID}) stored in the database.
+     */
+    Iterable<Integer> getAllMapIDs();
+
+    /**
+     * Returns the names (mapObjects) of all MapIDs (type: {@link MapID} stored in the database.
+     *
+     * @return  An {@link Iterable} of all the names (mapObjects)
+     *          of all MapIDs (type: {@link MapID}) stored in the database.
+     */
+    Iterable<String> getAllMapIDsName();
 
 }
