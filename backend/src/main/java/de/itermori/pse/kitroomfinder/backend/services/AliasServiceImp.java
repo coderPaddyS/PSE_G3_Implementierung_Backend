30222c5b5afa544ff53323bb3b781b16b8e5e73b
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-17 09:28:44 +0100

    Add javadoc to 'AliasServiceImp'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/AliasServiceImp.java
@@ -1,28 +1,45 @@
 package de.itermori.pse.kitroomfinder.backend.services;
 
 import de.itermori.pse.kitroomfinder.backend.models.Alias;
-import de.itermori.pse.kitroomfinder.backend.models.MapID;
 import de.itermori.pse.kitroomfinder.backend.models.Version;
 import de.itermori.pse.kitroomfinder.backend.repositories.AliasRepository;
 import de.itermori.pse.kitroomfinder.backend.repositories.VersionRepository;
-import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Provides a service for the model {@link Alias}.
+ * Implements the service interface which defines the corresponding GraphQL schema methods.
+ * Uses the repositories {@link AliasRepository}, {@link VersionRepository}.
+ *
+ * @author Lukas Zetto
+ * @author Adriano Castro
+ * @version 1.0
+ */
 @Service
 public class AliasServiceImp implements AliasService {
 
-    private AliasRepository aliasRepository;
-    private VersionRepository versionRepository;
+    private final AliasRepository aliasRepository;
+    private final VersionRepository versionRepository;
 
+    /**
+     * The constructor which initializes the alias service implementation
+     * with the required repositories.
+     *
+     * @param aliasRepository   The required {@link AliasRepository}.
+     * @param versionRepository The required {@link VersionRepository}.
+     */
     @Autowired
     public AliasServiceImp(AliasRepository aliasRepository, VersionRepository versionRepository) {
         this.aliasRepository = aliasRepository;
         this.versionRepository = versionRepository;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     @Override
     public boolean addAlias(String alias, int mapID, String mapObject) {
@@ -42,26 +59,41 @@ public class AliasServiceImp implements AliasService {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterable<Alias> getAlias(int mapID) {
         return aliasRepository.findByMapID(mapID);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterable<Alias> getAllAliases() {
         return aliasRepository.findAll();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterable<Alias> getAliasUpdates(int version) {
         return aliasRepository.findUpdatesByVersion(version);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getAmountEntriesAlias() {
         return String.valueOf(aliasRepository.count());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional
     @Override
     public boolean removeAlias(String name) {
