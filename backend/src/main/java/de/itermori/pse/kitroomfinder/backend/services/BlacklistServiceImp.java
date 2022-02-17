a3a7e3c4043ed6c8f5b7dbe69633c7a9f66d991e
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-17 10:47:44 +0100

    Add javadoc to 'BlacklistServiceImp'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/BlacklistServiceImp.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/services/BlacklistServiceImp.java
@@ -9,26 +9,47 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Provides a service for the model {@link BlacklistEntry}.
+ * Implements the service interface which defines the corresponding GraphQL schema methods.
+ * Uses the repository {@link BlacklistRepository}.
+ *
+ * @author Lukas Zetto
+ * @author Adriano Castro
+ * @version 1.0
+ */
 @Service
 public class BlacklistServiceImp implements BlacklistService{
 
-    private BlacklistRepository blacklistRepository;
+    private final BlacklistRepository blacklistRepository;
 
+    /**
+     * The constructor which initializes the alias service implementation
+     * with the required repositories.
+     *
+     * @param blacklistRepository   The required {@link BlacklistRepository}.
+     */
     @Autowired
     public BlacklistServiceImp(BlacklistRepository blacklistRepository) {
         this.blacklistRepository = blacklistRepository;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional
     @Override
     public boolean addToBlacklist(String toBlacklist) {
         if (isBlacklisted(toBlacklist)) {
-            return false;
+            return true;
         }
         blacklistRepository.save(new BlacklistEntry(toBlacklist));
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Transactional
     @Override
     public boolean removeFromBlacklist(String blacklistedToRem) {
@@ -36,6 +57,9 @@ public class BlacklistServiceImp implements BlacklistService{
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isBlacklisted(String word) {
         ExampleMatcher matcher = ExampleMatcher.matching()
@@ -45,6 +69,9 @@ public class BlacklistServiceImp implements BlacklistService{
         return blacklistRepository.exists(blacklistEntryExample);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Iterable<String> getBlacklist() {
         Stream<String> stream = blacklistRepository.findAll().parallelStream()
@@ -52,6 +79,9 @@ public class BlacklistServiceImp implements BlacklistService{
         return stream::iterator;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getAmountEntriesBlacklist() {
         return String.valueOf(blacklistRepository.count());
