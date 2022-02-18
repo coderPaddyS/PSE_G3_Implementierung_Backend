6ba128354d4f6086d8456e3b7799b4bfb0691f76
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-18 23:36:30 +0100

    Add javadocs to 'BackendApplication', 'ErrorHandling'

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/BackendApplication.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/BackendApplication.java
@@ -5,11 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * The main class.
+ *
+ * @author Lukas Zetto
+ * @author Adriano Castro
+ * @version 1.0
+ */
 @EnableEncryptableProperties
 @EnableJpaRepositories(basePackages = "de.itermori.pse.kitroomfinder.backend.repositories")
 @SpringBootApplication()
 public class BackendApplication {
 
+	/**
+	 * The main method.
+	 * @param args	The arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
