73815cbc530bdcf2cac0a18d5b581235c55fd1b7
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-18 21:54:39 +0100

    Add javadoc to 'UserRepositoryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/UserRepositoryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/repositories/UserRepositoryTest.java
@@ -1,27 +1,36 @@
 package de.itermori.pse.kitroomfinder.backend.repositories;
 
 import de.itermori.pse.kitroomfinder.backend.models.User;
-import java.util.HashSet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Test class for {@link UserRepository}.
+ *
+ * @author Adriano Castro
+ * @version 1.0
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Sets the test resources up.
+     */
     @BeforeEach
     void setUp() {
         userRepository.deleteAll();
     }
 
+    /**
+     * Tests the method {@link UserRepository#findByName(String)}.
+     */
     @Test
     void whenUserSaved_thenFindByName() {
         // save User to the database
@@ -31,12 +40,6 @@ class UserRepositoryTest {
         // check if saved user is found
         User actualUser = userRepository.findByName(expectedUser.getName());
         assertEquals(expectedUser, actualUser);
-    }
-
-    @Test
-    void test() {
-        User expectedUser = new User("Name", "USER");
-        userRepository.save(expectedUser);
     }
 
 }
