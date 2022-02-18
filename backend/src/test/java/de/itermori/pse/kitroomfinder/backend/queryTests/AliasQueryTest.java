e66b96adb08312c2f55408aa5f4a99643431a3a5
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-18 22:29:35 +0100

    Add javadoc to 'AliasQueryTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/queryTests/AliasQueryTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/queryTests/AliasQueryTest.java
@@ -1,42 +1,58 @@
 package de.itermori.pse.kitroomfinder.backend.queryTests;
 
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
-import de.itermori.pse.kitroomfinder.backend.models.*;
+import de.itermori.pse.kitroomfinder.backend.models.Alias;
+import de.itermori.pse.kitroomfinder.backend.models.DeletedAlias;
+import de.itermori.pse.kitroomfinder.backend.models.User;
+import de.itermori.pse.kitroomfinder.backend.models.Version;
 import de.itermori.pse.kitroomfinder.backend.repositories.AliasRepository;
 import de.itermori.pse.kitroomfinder.backend.repositories.DeletedAliasRepository;
 import de.itermori.pse.kitroomfinder.backend.repositories.UserRepository;
 import de.itermori.pse.kitroomfinder.backend.repositories.VersionRepository;
+import de.itermori.pse.kitroomfinder.backend.resolvers.mutationresolver.AliasMutation;
+import de.itermori.pse.kitroomfinder.backend.resolvers.queryresolver.AliasQuery;
+import java.io.IOException;
 import org.json.JSONException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Tests {@link AliasQuery}, {@link AliasMutation} (integration test).
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AliasQueryTest {
+class AliasQueryTest {
 
     @Autowired
     private GraphQLTestTemplate graphQLTestTemplate;
 
     @Autowired
-    VersionRepository versionRepository;
+    private VersionRepository versionRepository;
 
     @Autowired
-    AliasRepository aliasRepository;
+    private AliasRepository aliasRepository;
 
     @Autowired
-    DeletedAliasRepository deletedAliasRepository;
+    private DeletedAliasRepository deletedAliasRepository;
 
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
+    /**
+     * Cleans up the test resources.
+     */
     @AfterEach
-    void setAllUp() {
+    void cleanUp() {
         userRepository.deleteAll();
     }
 
+    /**
+     * Sets up the test resources.
+     */
     @BeforeEach
     void setUp(){
         userRepository.save(new User(UtilTests.USER, UtilTests.USER_AUTHORITY));
@@ -46,45 +62,88 @@ public class AliasQueryTest {
         versionRepository.deleteAll();
     }
 
+    /**
+     * Tests the method {@link AliasQuery#getAlias(int)}.
+     */
     @Test
-    public void getAliasTest() throws IOException, JSONException {
-        String testname = "getAlias";
-        aliasRepository.save(new Alias("alias", 1, "50.34", 1));
-        UtilTests.validate(graphQLTestTemplate, testname);
+    void getAliasTest() {
+        try {
+            String testname = "getAlias";
+            aliasRepository.save(new Alias("alias", 1, "50.34", 1));
+            UtilTests.validate(graphQLTestTemplate, testname);
+        } catch (IOException | JSONException e) {
+            fail(e.getMessage());
+        }
     }
 
+    /**
+     * Tests the method {@link AliasMutation#removeAlias(String)}.
+     */
     @Test
-    public void removeAliasTest() throws JSONException, IOException {
-        String testname = "removeAlias";
-        aliasRepository.save(new Alias("dalias", 1 , "50.34", 1));
-        UtilTests.validate(graphQLTestTemplate, testname, UtilTests.ADMIN);
+    void removeAliasTest() {
+        try {
+            String testname = "removeAlias";
+            aliasRepository.save(new Alias("dalias", 1, "50.34", 1));
+            UtilTests.validate(graphQLTestTemplate, testname, UtilTests.ADMIN);
+        } catch (IOException | JSONException e) {
+            fail(e.getMessage());
+        }
     }
 
+    /**
+     * Tests the method {@link AliasQuery#getNewAliases(int)}.
+     */
     @Test
-    public void getNewAliasesTest() throws JSONException, IOException {
-        String testname = "getNewAliases";
-        aliasRepository.save(new Alias("alias2", 1, "50.34", 2));
-        UtilTests.validate(graphQLTestTemplate, testname);
+    void getNewAliasesTest() {
+        try {
+            String testname = "getNewAliases";
+            aliasRepository.save(new Alias("alias2", 1, "50.34", 2));
+            UtilTests.validate(graphQLTestTemplate, testname);
+        } catch (IOException | JSONException e) {
+            fail(e.getMessage());
+        }
     }
 
+    /**
+     * Tests the method {@link AliasQuery#getNewDeletedAliases(int)}.
+     */
     @Test
-    public void getNewDeletedAliasesTest() throws JSONException, IOException {
-        String testname = "getNewDeletedAliases";
-        deletedAliasRepository.save(new DeletedAlias("dalias2", 1, 2));
-        UtilTests.validate(graphQLTestTemplate, testname);
+    void getNewDeletedAliasesTest() throws JSONException, IOException {
+        try {
+            String testname = "getNewDeletedAliases";
+            deletedAliasRepository.save(new DeletedAlias("dalias2", 1, 2));
+            UtilTests.validate(graphQLTestTemplate, testname);
+        } catch (IOException | JSONException e) {
+            fail(e.getMessage());
+        }
     }
 
+    /**
+     * Tests the method {@link AliasQuery#getVersion()}.
+     */
     @Test
-    public void getVersionNotInitiated() throws JSONException, IOException {
-        String testname = "getVersion";
-        UtilTests.validate(graphQLTestTemplate, testname);
+    void getVersionNotInitiated() {
+        try {
+            String testname = "getVersion";
+            UtilTests.validate(graphQLTestTemplate, testname);
+        } catch (IOException | JSONException e) {
+            fail(e.getMessage());
+        }
     }
 
+    /**
+     * Tests the method {@link AliasQuery#getVersion()}.
+     */
     @Test
-    public  void getVersionInitiated() throws JSONException, IOException {
-        String testname = "getVersionInitiated";
-        versionRepository.save(new Version(1));
-        versionRepository.incrementVersion();
-        UtilTests.validate(graphQLTestTemplate, testname);
+    void getVersionInitiated() {
+        try {
+            String testname = "getVersionInitiated";
+            versionRepository.save(new Version(1));
+            versionRepository.incrementVersion();
+            UtilTests.validate(graphQLTestTemplate, testname);
+        } catch (IOException | JSONException e) {
+            fail(e.getMessage());
+        }
     }
+
 }
