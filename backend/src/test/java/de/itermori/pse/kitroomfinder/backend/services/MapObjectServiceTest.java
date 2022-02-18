5e72cbe38095e2724c764112ee11795bc6608a35
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-02-18 22:15:47 +0100

    Remove unnecessary new lien in 'MapObjectServiceTest'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/services/MapObjectServiceTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/services/MapObjectServiceTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 /**
  * Test class for {@link MapObjectService}.
  *
