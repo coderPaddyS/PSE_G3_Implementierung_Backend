044c4d0932abb93a6a4fb86931b07d34539469af
Lukas Zetto <84440453+lukas333333@users.noreply.github.com>
2022-03-02 16:44:27 +0100

    log

--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/BackendApplication.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/BackendApplication.java
@@ -3,8 +3,15 @@ package de.itermori.pse.kitroomfinder.backend;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.time.Clock;
+import java.time.ZoneId;
+
+@Configuration
 @EnableEncryptableProperties
 @EnableJpaRepositories(basePackages = "de.itermori.pse.kitroomfinder.backend.repositories")
 @SpringBootApplication()
@@ -14,4 +21,10 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public Clock getClock() {
+		return Clock.systemDefaultZone();
+	}
+
 }
+
--- backend/src/main/java/de/itermori/pse/kitroomfinder/backend/instrumentation/RequestLogging.java
+++ backend/src/main/java/de/itermori/pse/kitroomfinder/backend/instrumentation/RequestLogging.java
@@ -1,39 +1,72 @@
 package de.itermori.pse.kitroomfinder.backend.instrumentation;
 
 
+import de.itermori.pse.kitroomfinder.backend.BackendApplication;
 import graphql.execution.instrumentation.InstrumentationContext;
 import graphql.execution.instrumentation.SimpleInstrumentation;
 import graphql.execution.instrumentation.SimpleInstrumentationContext;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 import graphql.ExecutionResult;
 import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
 
+@Import(BackendApplication.class)
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class RequestLogging extends SimpleInstrumentation {
 
+    @Autowired
+    private final Clock clock;
+
+    Boolean aBoolean = new File("target/log").mkdirs();
+    Path file = Paths.get("target/log/serverLog.txt");
 
     @Override
     public InstrumentationContext<ExecutionResult> beginExecution(
             InstrumentationExecutionParameters parameters) {
-        var executionId = parameters.getExecutionInput().getExecutionId();
-        log.info("{}: query: {} with variables: {}", executionId, parameters.getQuery(),
-                parameters.getVariables());
+        List<String> lines = new ArrayList<>();
+        Instant start = Instant.now(clock);
+        String receivedLog = "query received at " + start;
+        lines.add(receivedLog);
         return SimpleInstrumentationContext.whenCompleted((executionResult, throwable) -> {
+            Instant finishedTime = Instant.now(clock);
+            Duration duration = Duration.between(start, finishedTime);
             if (throwable == null) {
-                log.info("{}: succeeded {}", executionId, throwable);
+                String successLog = "query finished successfully at " + finishedTime +
+                        "(duration: " + duration + ")";
+                lines.add(successLog);
             } else {
-                log.warn("{}: failed in {}", executionId, throwable);
+                String failureLog = "query failed at " + finishedTime + "with Exception: " + throwable +
+                        "(duration: " + duration + ")";
+                lines.add(failureLog);
+            }
+            try {
+                Files.write(file, lines, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
     }
