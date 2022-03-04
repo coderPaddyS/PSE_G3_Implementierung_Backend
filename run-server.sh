69cbe9c6e5710833683c76ee6b6567ee582d97d1
Patrick Schneider <s.patrick.2001@web.de>
2022-03-04 21:00:56 +0100

    added missing .

--- run-server.sh
+++ run-server.sh
@@ -6,7 +6,7 @@ if [ "$1" ]
 then git checkout "$1"
 else git checkout main
 fi
-cd backend || exit && /mvnw package -DskipTests
+cd backend || exit && ./mvnw package -DskipTests
 cd .. || exit 
 docker build -t kit-finder .
 cd /opt/docker/kit-finder || exit
