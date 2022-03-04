e15c9e2afc0e9848ba6aa45a54667046aede3af7
Patrick Schneider <s.patrick.2001@web.de>
2022-03-04 13:54:00 +0100

    Fixed run-server and removed unnecessary port exposure

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM openjdk:17-alpine
 WORKDIR /app
 
 EXPOSE 7070
-EXPOSE 3306
 
 COPY backend/target/lib/* target/lib/
 COPY backend/target/backend-0.0.1-SNAPSHOT.jar target/app.jar
--- run-server.sh
+++ run-server.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+git fetch
+if [ "$1" ]
+then git checkout "$1"
+else git checkout main
+fi
 git pull
-cd backend && ./mvnw package -DskipTests
+cd backend || exit && ./mvnw package -DskipTests
 docker build -t kit-finder .
 cd /opt/docker/kit-finder || exit
 docker-compose up -d
