f56a70b28b740eacaa60f6d5578b97a49aef0c74
Patrick Schneider <s.patrick.2001@web.de>
2022-03-04 13:59:06 +0100

    Updated  order of operations

--- run-server.sh
+++ run-server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 git fetch
+git pull
 if [ "$1" ]
 then git checkout "$1"
 else git checkout main
 fi
-git pull
-cd backend || exit && ./mvnw package -DskipTests
+./backend/mvnw package -DskipTests || exit
 docker build -t kit-finder .
 cd /opt/docker/kit-finder || exit
 docker-compose up -d
