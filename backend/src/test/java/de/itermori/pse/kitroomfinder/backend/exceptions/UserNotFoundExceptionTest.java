3dd94a09fc0893f874c4cb1092cf6d04f5f301ab
Adriano Soares Castro <ubrqr@student.kit.edu>
2022-03-11 20:04:07 +0100

    Add javadocs to test classes of package 'exceptions'

--- backend/src/test/java/de/itermori/pse/kitroomfinder/backend/exceptions/UserNotFoundExceptionTest.java
+++ backend/src/test/java/de/itermori/pse/kitroomfinder/backend/exceptions/UserNotFoundExceptionTest.java
@@ -5,15 +5,27 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Test class for {@link UserNotFoundException}.
+ *
+ * @author Adriano Castro
+ * @version 1.0
+ */
 class UserNotFoundExceptionTest {
 
     private UserNotFoundException userNotFoundException;
 
+    /**
+     * Sets up the test resources.
+     */
     @BeforeEach
     void setUp() {
         userNotFoundException = new UserNotFoundException();
     }
 
+    /**
+     * Tests the method {@link UserNotFoundException#getMessage()}.
+     */
     @Test
     void testGetMessage() {
         String expected = "User not registered yet";
